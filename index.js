5f2c95bc1dbf3c816cf104bee56da543f46ce75e
Ruttapon Suppaakkarasolpon <ruttapon.sup@student.mahidol.edu>
2026-03-16 15:16:30 +0700

    test

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+const http = require("http");
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World from DevOps Lab!!55588\n');
-});
-server.listen(port, () => {
+const requestListener = (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World from DevOps Lab!\n");
+};
+
+const app = http.createServer(requestListener);
+
+// We only listen if this file is run directly, not when imported by tests
+if (require.main === module) {
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
     console.log(`Server running at port ${port}`);
-});
+  });
+}
+
+module.exports = app; // Export for testing
